c78b67d13a772ab859544182a83ccd6f47da2d42
Andrew Skinner <skinsster91@gmail.com>
2020-03-21 13:00:23 -0500

    do all of the fighting.... figure it out

--- themes/wp-monstro/header.php
+++ themes/wp-monstro/header.php
@@ -12,6 +12,7 @@
 
 <head>
   <?php wp_head(); ?>
+  <?php wp_enqueue_style('navbar', get_stylesheet_directory() . 'assets/dist/css/navbar.min.css' ) ?>
 </head>
 
 <body <?php body_class() ?>>
@@ -22,7 +23,7 @@
 
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#"><img src="/wp-content/uploads/2020/03/Montstro-logo.svg" alt="Monstro"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>

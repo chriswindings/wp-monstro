99f3e4b9502ffe06080f63c33907dbbef2546b00
Andrew Skinner <skinsster91@gmail.com>
2020-03-21 16:37:18 -0500

    scaffold partial, add data

--- themes/wp-monstro/models/home.php
+++ themes/wp-monstro/models/home.php
@@ -3,6 +3,11 @@ return [
   "hero" => [
     "preHeading" => "Your life, your wealth, your freedom:",
     "heading" => "It’s time for an entirely new perspective."
+  ],
+  "main" => [
+    "heading" => "It’s time to bring transparency to the wealth management industry.",
+    "copy" => "It’s time for an objective partner, focused solely on helping to make you money, save you money, protect your assets, protect you from risk, minimize your taxes and minimize your expenses.<br>It’s time for wealth management that actually works for you and with you.<br>This is what we stand for.",
+    "logoImageUrl" => "#"
   ]
 ]
 ?>

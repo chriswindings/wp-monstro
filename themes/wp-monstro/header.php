9f6059b56e9106d08a72fbb83ac8191c068ed956
Andrew Skinner <skinsster91@gmail.com>
2020-03-21 14:20:46 -0500

    update navbar styles

--- themes/wp-monstro/header.php
+++ themes/wp-monstro/header.php
@@ -23,21 +23,18 @@
 
 <header>
     <nav class="navbar navbar-expand-lg">
-    <a class="navbar-brand" href="#"><img src="/wp-content/uploads/2020/03/Montstro-logo.svg" alt="Monstro"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand mx-auto" href="#"><img src="/wp-content/uploads/2020/03/Montstro-logo.svg" alt="Monstro"></a>
+    <button class="navbar navbar-toggleable-sm" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
         <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="#">Waitlist <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            <a class="nav-link" href="#">Careers</a>
         </li>
         </ul>
     </div>

efbccc4817c593ef1acd0734dffd0bd7796b22c4
Chris Windings <chris.windings@daveramsey.com>
2020-03-21 17:09:30 -0500

    add typography override

--- themes/wp-monstro/functions.php
+++ themes/wp-monstro/functions.php
@@ -3,6 +3,7 @@ wp_enqueue_style('global', get_stylesheet_directory_uri() . '/assets/dist/css/gl
 wp_enqueue_style('navbar', get_stylesheet_directory_uri() . '/assets/dist/css/navbar.min.css' );
 wp_enqueue_style('home-page', get_stylesheet_directory_uri() . '/assets/dist/css/home.min.css' );
 wp_enqueue_style('main', get_stylesheet_uri() );
+wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans|Red+Hat+Display:700|Red+Hat+Text:400,700&display=swap');
 
 // wp_enqueue_style( 'bootstrap', '/wp-content/themes/wp-monstro/node_modules/bootstrap/dist/css/bootstrap.min.css');
 wp_enqueue_script('bootstrap', '/wp-content/themes/wp-monstro/node_modules/bootstrap/dist/js/bootstrap.bundle.js', array('jquery'));

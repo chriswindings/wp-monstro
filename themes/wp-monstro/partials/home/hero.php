bc95bee5ff57be7923f2047b25caa923d19a961e
Andrew Skinner <skinsster91@gmail.com>
2020-03-21 16:09:23 -0500

    desctructuring?

--- themes/wp-monstro/partials/home/hero.php
+++ themes/wp-monstro/partials/home/hero.php
@@ -8,12 +8,13 @@
  * @since 1.0.0
  */
 $content = require(get_template_directory() . "/models/home.php");
+$heroContent = $content["hero"]
 ?>
 
 <section class="HomeHero">
   <div class="HomeHero-content">
-    <p class="HomeHero-preHeading"><?php echo $content["hero"]["preHeading"] ?></p>
-    <h1 class="HomeHero-heading"><?php echo $content["hero"]["heading"] ?></h1>
+    <p class="HomeHero-preHeading"><?php echo $heroContent["preHeading"] ?></p>
+    <h1 class="HomeHero-heading"><?php echo $heroContent["heading"] ?></h1>
   </div>
   <button type="button" class="HomeHero-cta btn btn-primary">Let us Show You</button>
 </section>

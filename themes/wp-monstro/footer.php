87190c0562de95b9cbb3a0a6dc2e1d363e0cc2fc
Andrew Skinner <skinsster91@gmail.com>
2020-03-21 18:01:59 -0500

    WIP: footer

--- themes/wp-monstro/footer.php
+++ themes/wp-monstro/footer.php
@@ -9,8 +9,22 @@
 
  ?>
 
- <footer>
-  <span>You should implement a footer partial</span>
+ <footer class="Footer container">
+    <button class="navbar navbar-toggleable-sm collapsed" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <img class="Footer-logo" src="/wp-content/uploads/2020/03/Monstro.svg" alt="Monstro Logo">
+    <div class="Footer-socialIcons">
+      <p>Don't forget to add those social icons.</p>
+    </div>
+    <div class="Footer-links">
+      <a class="Footer-link" href="#">Privacy Policy</a>
+      <span class="Footer-divider">|</span>
+      <a class="Footer-link" href="#">Disclosures</a>
+    </div>
+    <div class="Footer-copyright">
+      <span class="Footer-copyrightText">Copyright © 2020 Monstro, LLC. All Rights Reserved</span>
+    </div>
  </footer>
 
 <?php wp_footer(); ?>

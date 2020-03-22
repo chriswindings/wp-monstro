99edf370a1eaf4cdd4294fd0543755f69b565f7f
Andrew Skinner <skinsster91@gmail.com>
2020-03-22 08:44:13 -0500

    change image on desktop nav

--- themes/wp-monstro/header.php
+++ themes/wp-monstro/header.php
@@ -23,14 +23,17 @@
 ?>
 
 <header>
-    <nav class="navbar navbar-expand-lg">
-            <a class="navbar-brand mx-auto" href="/"><img src="/wp-content/uploads/2020/03/Montstro-logo.svg" alt="Monstro"></a>
-            <button class="navbar-toggleable-sm collapsed" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-md">
+            <a class="navbar-brand mx-auto" href="/">
+                <img class="d-md-none" src="/wp-content/uploads/2020/03/Montstro-logo.svg" alt="Monstro">
+                <img class="d-sm-none d-md-block" src="/wp-content/uploads/2020/03/nav-logo-desktop.svg" alt="Monstro">
+            </a>
+            <button class="navbar-toggleable-sm collapsed d-md-none" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul class="navbar-nav mr-auto mt-lg-0">
+            <ul class="navbar-nav mr-auto mt-md-0">
                 <li class="nav-item active">
                     <a class="nav-link" href="/waitlist">Waitlist <span class="sr-only">(current)</span></a>
                 </li>
@@ -38,7 +41,7 @@
                     <a class="nav-link" href="/careers">Careers</a>
                 </li>
             </ul>
-            <div class="mb-5">
+            <div class="mb-5 d-md-none">
                 <a href="#" class="ml-1 mr-1"><img src="/wp-content/uploads/2020/03/faceook-icon.svg" alt="facebook"></img></a>
                 <a href="#" class="ml-1 mr-1"><img src="/wp-content/uploads/2020/03/twitter-icon.svg" alt="twitter"></img></a>
                 <a href="#" class="ml-1 mr-1"><img src="/wp-content/uploads/2020/03/linkedIn-icon.svg" alt="linkedIn"></img></a>

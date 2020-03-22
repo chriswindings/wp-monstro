5fdfa5de0f3dbd54c339bdf528836572908cf1d8
Chris Windings <chris.windings@daveramsey.com>
2020-03-22 18:14:31 -0500

    remove unused js

--- themes/wp-monstro/functions.php
+++ themes/wp-monstro/functions.php
@@ -7,6 +7,4 @@ wp_enqueue_style('careers-page', get_stylesheet_directory_uri() . '/assets/dist/
 wp_enqueue_style('main', get_stylesheet_uri() );
 wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans|Red+Hat+Display:700|Red+Hat+Text:400,700&display=swap');
 
-// wp_enqueue_style( 'bootstrap', '/wp-content/themes/wp-monstro/node_modules/bootstrap/dist/css/bootstrap.min.css');
 wp_enqueue_script('bootstrap', '/wp-content/themes/wp-monstro/node_modules/bootstrap/dist/js/bootstrap.bundle.js', array('jquery'));
-wp_enqueue_script('global', '/wp-content/themes/wp-monstro/assets/dist/js/global.min.js');

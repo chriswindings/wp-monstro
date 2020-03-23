d3a6bb0ea0e66b787d6eeb3581dc46034a37533b
Andrew Skinner <skinsster91@gmail.com>
2020-03-22 21:39:54 -0500

    finalize mobil career page

--- themes/wp-monstro/partials/careers/single/content.php
+++ themes/wp-monstro/partials/careers/single/content.php
@@ -9,58 +9,126 @@
  */
 ?>
 
-<section class="CareersContent"></section>
+<section class="CareersContent">
   <div class="accordion" id="accordionExample">
     <div class="CareersContent-card card">
-      <div id="headingOne">
-        <h2 class="CareersContent-accordianHeading">
-          <button class="CareersContent-accordianHeadingButton" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            Collapsible Group Item #1
+      <div id="headingOne" class="CareersContent-cardContainer container">
+        <a class="CareersContent-accordianHeadingButton" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          <h2 class="CareersContent-accordianHeading">
+            <span>About the Position</span>
             <img class="CareersContent-accordianHeadingImage CareersContent-accordianHeadingImage--open" src="/wp-content/uploads/2020/03/accordian-open.svg">
             <img class="CareersContent-accordianHeadingImage CareersContent-accordianHeadingImage--closed" src="/wp-content/uploads/2020/03/accordian-closed.svg">
-          </button>
-        </h2>
-      </div>
+          </h2>
+        </a>
+        </div>
 
       <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
         <div class="CareersContent-cardBody card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          <?php the_field('aboutThePosition') ?>
         </div>
       </div>
     </div>
 
     <div class="CareersContent-card card">
-      <div id="headingTwo">
-        <h2 class="CareersContent-accordianHeading">
-          <button class="CareersContent-accordianHeadingButton collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            Collapsible Group Item #2
+      <div id="headingTwo" class="CareersContent-cardContainer container">
+        <a class="CareersContent-accordianHeadingButton collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          <h2 class="CareersContent-accordianHeading">
+            <span>Minimum Qualifications</span>
             <img class="CareersContent-accordianHeadingImage CareersContent-accordianHeadingImage--open" src="/wp-content/uploads/2020/03/accordian-open.svg">
             <img class="CareersContent-accordianHeadingImage CareersContent-accordianHeadingImage--closed" src="/wp-content/uploads/2020/03/accordian-closed.svg">
-          </button>
-        </h2>
+          </h2>
+        </a>
       </div>
       <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
         <div class="CareersContent-cardBody card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+        <?php 
+          $rows = get_field('minimumQualifications');
+          if($rows)
+          {
+            echo '<ul>';
+
+            foreach($rows as $row)
+            {
+              ?>
+              <li class="CareersContent-listItem"><?php echo $row["minimunQualification"] ?>
+              <?php
+            }
+
+            echo '</ul>';
+          }
+          ?>
         </div>
       </div>
     </div>
 
     <div class="CareersContent-card card">
-      <div id="headingThree">
-        <h2 class="CareersContent-accordianHeading">
-          <button class="CareersContent-accordianHeadingButton collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-            Collapsible Group Item #3
+      <div id="headingThree" class="CareersContent-cardContainer container">
+        <a class="CareersContent-accordianHeadingButton collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          <h2 class="CareersContent-accordianHeading">
+            <span>Preferred Qualifications</span>
             <img class="CareersContent-accordianHeadingImage CareersContent-accordianHeadingImage--open" src="/wp-content/uploads/2020/03/accordian-open.svg">
             <img class="CareersContent-accordianHeadingImage CareersContent-accordianHeadingImage--closed" src="/wp-content/uploads/2020/03/accordian-closed.svg">
-          </button>
-        </h2>
+          </h2>
+        </a>
       </div>
       <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
         <div class="CareersContent-cardBody card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+        <?php 
+          $rows = get_field('preferredQualifications');
+          if($rows)
+          {
+            echo '<ul>';
+
+            foreach($rows as $row)
+            {
+              ?>
+              <li class="CareersContent-listItem"><?php echo $row["preferredQualification"] ?>
+              <?php
+            }
+
+            echo '</ul>';
+          }
+          ?>
         </div>
       </div>
     </div>
-</div>
+
+    <div class="CareersContent-card card">
+      <div id="headingFour" class="CareersContent-cardContainer container">
+        <a class="CareersContent-accordianHeadingButton collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+          <h2 class="CareersContent-accordianHeading">
+          <span>Benefits</span>
+            <img class="CareersContent-accordianHeadingImage CareersContent-accordianHeadingImage--open" src="/wp-content/uploads/2020/03/accordian-open.svg">
+            <img class="CareersContent-accordianHeadingImage CareersContent-accordianHeadingImage--closed" src="/wp-content/uploads/2020/03/accordian-closed.svg">
+          </h2>
+        </a>
+      </div>
+      <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+        <div class="CareersContent-cardBody card-body">
+        <?php 
+          $rows = get_field('benefits');
+          if($rows)
+          {
+            echo '<ul>';
+
+            foreach($rows as $row)
+            {
+              ?>
+              <li class="CareersContent-listItem"><?php echo $row["benefit"] ?>
+              <?php
+            }
+
+            echo '</ul>';
+          }
+          ?>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container CareersContent-cta">
+    <a href="<?php the_field("linkedInLink") ?>" class="CareersContent-linkedIn btn btn-primary">
+      <img class="CareersContent-linkedInImage" src="/wp-content/uploads/2020/03/icon-btn-applyLI@3x.svg">
+      Apply with LinkedIn
+    </a>
+  </div>
 </section>

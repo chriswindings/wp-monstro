a4acd47de3a6326bbd3b953d3aedbd87929a5d0a
Andrew Skinner <skinsster91@gmail.com>
2020-03-24 12:36:07 -0500

    add gtm script

--- themes/wp-monstro/assets/src/js/global.js
+++ themes/wp-monstro/assets/src/js/global.js
@@ -1,1 +1,10 @@
 import "scroll-behavior-polyfill";
+window.dataLayer = window.dataLayer || [];
+
+  function gtag(){dataLayer.push(arguments);}
+
+  gtag('js', new Date());
+
+ 
+
+  gtag('config', 'UA-161742203-1');
